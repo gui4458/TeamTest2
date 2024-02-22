8a1fcabe1e906bad33c5d4c94111aaebb2fe247f
gui4458 <gui4458@gmail.com>
2024-02-22 16:47:03 +0900

    첫번째

---
+++ src/test/java/com/green/TeamTest2/TeamTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.TeamTest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
